0af1d1687ac30bce37d6562b514a08a4900ffa9b
akhyari <muhammadraffi.akhyari@gmail.com>
2021-06-09 14:23:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gamelist/DetailGame.kt
@@ -0,0 +1,11 @@
+package com.example.gamelist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_game)
+    }
+}
