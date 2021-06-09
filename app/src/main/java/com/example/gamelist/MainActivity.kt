acaf81007bce55e8b928607e110ce7594a0cb669
akhyari <muhammadraffi.akhyari@gmail.com>
2021-06-09 16:27:55 +0700

    + Edit Gradle - MainActivity.kt

--- app/src/main/java/com/example/gamelist/MainActivity.kt
+++ app/src/main/java/com/example/gamelist/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.gamelist
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            }
         setContentView(R.layout.activity_main)
     }
 }
